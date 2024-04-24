9a2efe5f07b44960cf3b7a32be3fcc3edf2217fa
SoulThalion <adrielguijarro@gmail.com>
2024-04-24 18:35:52 +0100

    database conection and basic structure

---
+++ api/controllers/middleware/models/user.model.js
@@ -0,0 +1,64 @@
+const { DataTypes } = require('sequelize')
+const { sequelize } = require('../../database')
+
+const User = sequelize.define(
+	'user',
+	{
+		userName: {
+			type: DataTypes.STRING(20),
+			allowNull: false
+		},
+
+        name: {
+			type: DataTypes.STRING(),
+			allowNull: false
+		},
+
+        surName: {
+			type: DataTypes.STRING(),
+			allowNull: false
+		},
+
+        telephone: {
+			type: DataTypes.CHAR(9),
+			allowNull: false
+		},
+		
+		email: {
+            type: DataTypes.STRING(40),
+            allowNull: false,
+			validate: {
+				isEmail: true
+			}
+        },
+
+		role: {
+            type: DataTypes.ENUM('mechanic', 'admin', 'manager'),
+            allowNull: false,
+			defaultValue: 'mechanic'
+        },
+
+		password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+        },
+    },
+	{	
+		timestamps: false,
+		indexes: [
+			{
+				unique: true,
+				allowNull: false,
+				fields: ['userName']
+			},
+
+			{
+				unique: true,
+				allowNull: false,
+				fields: ['email']
+			}
+		]
+	},
+)
+
+module.exports = User
---
+++ database/relations.js
@@ -0,0 +1,131 @@
+/*const User = require('../api/models/user.model')*/
+
+
+//Many to Many
+function addRelationsToModels() {
+	/*try {
+
+		User.belongsToMany(Game, {
+			through: 'Favorites',
+    		as: 'Favorite',
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+
+		})
+
+		Game.belongsToMany(User, {
+			through: 'Favorites',
+    		as: 'Favorite',
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+
+		})
+
+		User.belongsToMany(Game, {
+			through: 'userGames',
+
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		})
+
+		Game.belongsToMany(User, {
+			through: 'userGames',
+
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		})
+
+		User.belongsToMany(Game, {
+			through: Master,
+			as: 'MastersGame',
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		})
+
+		Game.belongsToMany(User, {
+			through: Master,
+			as: 'MastersGame',
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		})
+
+		Theme.belongsToMany(Game, {
+			through: 'gameThemes',
+
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		})
+
+		Game.belongsToMany(Theme, {
+			through: 'gameThemes',
+
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		})
+
+		Session.belongsTo(Master);
+		Master.hasMany(Session);
+
+		Session.belongsTo(Game);
+		Game.hasMany(Session);
+
+
+		Session.belongsToMany(User, {
+			through: SessionPlayer,
+
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		})
+
+		User.belongsToMany(Session, {
+			through: SessionPlayer,
+
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		})
+
+		Game.hasOne(Inventory, { onDelete: 'cascade', onUpdate: 'cascade' });
+		Inventory.belongsTo(Game, { onDelete: 'cascade', onUpdate: 'cascade' });
+
+
+
+		/*User.belongsTo(Country);
+		Country.hasMany(User);
+
+
+		User.hasOne(Address, { onDelete: 'cascade', onUpdate: 'cascade' });
+		Address.belongsTo(User, { onDelete: 'cascade', onUpdate: 'cascade' });
+
+
+		Actor.belongsToMany(Movie, {
+			through: 'ActorMovie',
+			as: 'ActorMovies',
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		});
+		Movie.belongsToMany(Actor, {
+			through: 'ActorMovie',
+			as: 'ActorMovies',
+			onDelete: 'cascade',
+			onUpdate: 'cascade',
+			timestamps: false
+		});
+
+		console.log('Relations added to all models');
+	} catch (error) {
+		throw error
+	}*/
+}
+
+module.exports = addRelationsToModels
