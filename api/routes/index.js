f5e232db6a11fb82ac98f5195bf6063ff6d94c06
SoulThalion <adrielguijarro@gmail.com>
2024-05-03 19:13:44 +0100

    order añadido

--- api/routes/index.js
+++ api/routes/index.js
@@ -4,8 +4,8 @@ router.use('/user', require('./user.router'))
 router.use('/auth', require('./auth.router'))
 router.use('/client', require('./client.router'))
 router.use('/ship', require('./ship.router'))
-/*router.use('/inventory', require('./inventory.router'))
-router.use('/session', require('./session.router'))
+router.use('/order', require('./order.router'))
+/*router.use('/session', require('./session.router'))
 router.use('/master', require('./master.router'))
 router.use('/sessionPlayer', require('./sessionPlayer.router'))*/
 
